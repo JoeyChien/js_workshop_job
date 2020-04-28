8d3b2be769b0f44e466af8b87d86466e5985dd92
Joey <aileen60222@gmail.com>
2020-04-28 16:43:34 +0800

    task3

--- job.js
+++ job.js
@@ -2,20 +2,37 @@ let form = document.forms[0];
 
 form.addEventListener("submit", function(e){
   e.preventDefault();
+  document.querySelector('#job-pannel').innerHTML = "";
   let description = document.querySelector('input[name="description"]').value;
   let location = document.querySelector('input[name="location"]').value;
   let full_time = document.querySelector('input[name="full_time"]').checked;
-  full_time = full_time ? "on" : ""
-  console.log(description, location, full_time)
+  full_time = full_time ? "on" : ""  
 
   axios.get('https://still-spire-37210.herokuapp.com/positions.json', { params: { description: `${description}`, location: `${location}`, full_time: `${full_time}`  } })
-    .then((res) => { console.table(res.data) })
+    .then((res) => {     
+      for(let i = 0; i < res.data.length; i++){
+        document.querySelector('#job-pannel').insertAdjacentHTML("beforeend",addTemplate(res.data[i]));  
+      }
+     })
     .catch((error) => { console.error(error) })
 
 }, false)
 
-
-
-
+function addTemplate(data){
+  return `<tr>
+  <td>
+    <h4><a href="${data.url}"> ${data.title}</a></h4>
+    <p class="source">
+    <a class="company" href="${data.company_url}">${data.company}</a>
+    –
+    <strong class="fulltime">${data.type}</strong>
+    </p>
+  </td>
+  <td class="meta">
+    <span class="location">${data.location}</span>
+  </td>
+</tr>`;
+  
+}
 
 
